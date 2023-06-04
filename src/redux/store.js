fc3850657bcf15c544ad6d0a7846cc227a05a50d
Swarp <m.kleban@terawatt-group.com>
2023-06-04 12:42:44 +0300

    created base project

--- src/redux/store.js
+++ src/redux/store.js
@@ -13,34 +13,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from '../redux/slice/contactsSlice';
 import { filterReducer } from '../redux/slice/filterSplice';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
-const persistConfig = {
-  key: 'contacts',
-  storage,
-};
 
 export const store = configureStore({
   reducer: {
     contacts: persistReducer(persistConfig, contactsReducer),
     filter: filterReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
